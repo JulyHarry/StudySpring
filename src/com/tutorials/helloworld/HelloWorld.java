a5e8da89e4f16042e0756e2624c4d372ec6e55c0
Hang.X <xuexunhao@gmail.com>
2019-09-09 23:32:30 +0800

    add w3cschool.tutorials.HelloWorld

---
+++ src/com/tutorials/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorials.helloworld;
+
+public class HelloWorld {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("Your Message is "+this.message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
